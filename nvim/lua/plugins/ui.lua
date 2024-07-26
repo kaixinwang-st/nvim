38ee6ca25c8388ec4999a6699f10036536f788d5
Kaixin Wang <kaixin.wang@sensortower.com>
2024-07-26 14:55:04 -0700

    feat: add fileline plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -101,6 +101,16 @@ return {
     },
   },
 
+  -- fileline
+  {
+    "b0o/incline.nvim",
+    config = function()
+      require("incline").setup()
+    end,
+    -- Optional: Lazy load Incline
+    event = "VeryLazy",
+  },
+
   {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
