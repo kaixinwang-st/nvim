5ce400d19251494518f631f9916442798972a05f
Kaixin Wang <kaixin.wang@sensortower.com>
2024-07-26 15:19:58 -0700

    feat: add filename plugin incline.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -104,11 +104,64 @@ return {
   -- fileline
   {
     "b0o/incline.nvim",
-    config = function()
-      require("incline").setup()
-    end,
-    -- Optional: Lazy load Incline
     event = "VeryLazy",
+    config = function()
+      local has_helpers, helpers = pcall(require, "incline.helpers")
+      local has_navic, navic = pcall(require, "nvim-navic")
+      local has_devicons, devicons = pcall(require, "nvim-web-devicons")
+
+      -- 使用默认对比颜色函数
+      local function get_contrast_color(color)
+        if has_helpers then
+          return helpers.contrast_color(color)
+        else
+          return "#FFFFFF" -- 回退到白色
+        end
+      end
+
+      -- 如果没有找到 incline.helpers 和 nvim-web-devicons 模块，提前返回避免错误
+      if not has_helpers or not has_devicons then
+        vim.notify("Required plugin modules not found: incline.helpers or nvim-web-devicons", vim.log.levels.WARN)
+        return
+      end
+
+      require("incline").setup({
+        window = {
+          padding = 0,
+          margin = { horizontal = 0, vertical = 0 },
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if filename == "" then
+            filename = "[No Name]"
+          end
+
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+
+          local res = {
+            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = get_contrast_color(ft_color) } or "",
+            " ",
+            { filename, gui = modified and "bold,italic" or "bold" },
+            guibg = "#44406e",
+          }
+
+          if props.focused and has_navic then
+            local navic_data = navic.get_data(props.buf)
+            for _, item in ipairs(navic_data or {}) do
+              table.insert(res, {
+                { " > ", group = "NavicSeparator" },
+                { item.icon, group = "NavicIcons" .. item.type },
+                { item.name, group = "NavicText" },
+              })
+            end
+          end
+
+          table.insert(res, " ")
+          return res
+        end,
+      })
+    end,
   },
 
   {
