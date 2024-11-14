df2b8879f5ee612e37e570beacfd44d369bc1f0d
Kaixin Wang <kaixin.wang@sensortower.com>
2024-11-14 11:19:42 +0800

    feat: neo-tree always show hidden files

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,26 @@
 return {
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {},
+    lazy = false,
+    config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            visible = true, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false, -- only works on Windows for hidden files/directories
+            hide_by_name = {
+              --"node_modules"
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  {
     "numToStr/Comment.nvim",
     opts = {},
     lazy = false,
